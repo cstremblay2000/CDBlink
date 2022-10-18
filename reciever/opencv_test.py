19c0fd0f77ae9455c66cc25d5ee065001b718d9d
cst <cstremblay2000@gmail.com>
2022-10-18 09:41:43 -0400

    got video and loggnig workign

--- reciever/opencv_test.py
+++ reciever/opencv_test.py
@@ -1,10 +1,18 @@
 import cv2 as cv
 import numpy as np
 import sys
+import logging
+
+VIDEO_PATH="./images/test.mp4"
+NAMED_WINDOW="w1"
 
 square = np.array([[1,1,1],
                    [1,1,1],
                    [1,1,1]])
+
+def init_logging():
+    logging.basicConfig( level=logging.DEBUG )
+    return 
 
 def mahalanobis(x=None, data=None, cov=None):
     x_mu = x - np.mean(data)
@@ -26,35 +34,58 @@ def multi_ero(im, num, element=square):
     return im
 
 def main():
+    # initialize logging
+    init_logging()
+
     # get image and filter noise
     img = cv.imread( sys.argv[1] )
     blur = cv.GaussianBlur( img, (3,3), 0 )
-    cv.imshow("Display window", blur )
-    k = cv.waitKey( 0 )
+    if( logging.root.level <= logging.DEBUG ):
+        cv.imshow("Display window", blur )
+        cv.waitKey( 0 )
 
     # split image into rgb channels
     b, g, r = cv.split( blur )
-    cv.imshow( "Display window", g )
-    k = cv.waitKey( 0 )
+    if( logging.root.level <= logging.DEBUG ):
+        cv.imshow( "Display window", g )
+        k = cv.waitKey( 0 )
 
     # binarize (turn into black and white) the green channel
     ret3,th3 = cv.threshold( g ,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
-    cv.imshow( "Display window", th3 )
-    k = cv.waitKey( 0 )
+    if( logging.root.level <= logging.DEBUG ):
+        cv.imshow( "Display window", th3 )
+        k = cv.waitKey( 0 )
 
     # connected component analysis
     connectivity = 4 
     output = cv.connectedComponentsWithStats( th3, connectivity, cv.CV_32S )
-    print( output[1] )
+    logging.debug( output[1] )
 
     # detect edges 
     edged = cv.Canny(th3, 30, 200)
-    cv.imshow( "edges", edged )
     contours, hierarchy = cv.findContours( edged,
     cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
     cv.drawContours( img, contours, -1, (0,255,0), 3 )
-    cv.imshow( 'Contours', img )
-    cv.waitKey( 0 )
+    if( logging.root.level <= logging.DEBUG ):
+        cv.imshow( "edges", edged )
+        cv.waitKey( 0 )
+        cv.imshow( 'Contours', img )
+        cv.waitKey( 0 )
+
+    # open video 
+    cap = cv.VideoCapture( VIDEO_PATH )
+    cv.namedWindow( NAMED_WINDOW, cv.WINDOW_NORMAL )
+    while( cap.isOpened() ):
+        ret, frame = cap.read()
+        if( not ret ):
+            print( "cant receive frame" )
+            break
+        
+        gray = cv.cvtColor( frame, cv.COLOR_BGR2GRAY )
+        if( logging.root.level <= logging.DEBUG ):
+            cv.imshow( NAMED_WINDOW, gray )
+            cv.waitKey( 0 )
+    cap.release()
 
 if( __name__ == "__main__" ):
     main()
